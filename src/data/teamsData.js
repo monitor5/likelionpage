a606acf490edf93d7630a0afd8f35662ce723a49
EunBin <rhdmsqls1234@naver.com>
2026-05-22 18:20:18 +0900

    [fix]

--- src/data/teamsData.js
+++ src/data/teamsData.js
@@ -2,6 +2,6 @@
 export const teamsData = [
   { id: 1, name: "또이겼조", members: ["강선후", "경태훈", "김정현", "이아란", "이지섭"], score: 0 },
   { id: 2, name: "이기조", members: ["고은빈", "이현진", "황지영", "김우현"], score: 0 },
-  { id: 3, name: "쭈삼조", members: ["엄태성", "백승효", "박규리", "이현진"], score: 0 },
+  { id: 3, name: "쭈삼조", members: ["엄태성", "백승효", "박규리", "이해솔"], score: 0 },
   { id: 4, name: "war(워)", members: ["이효원", "박현지", "오채민", "강연성"], score: 0 },
 ];
--- src/pages/ScoreBoard.jsx
+++ src/pages/ScoreBoard.jsx
@@ -10,7 +10,7 @@ const Scoreboard = ({ teams, onUpdateScore }) => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
         {teams.map((team) => (
           <ScoreCard 
             key={team.id}
